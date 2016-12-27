d7463f3c001f0898ee59dc05a2087323afc478e9
houdunwang <2300071698@qq.com>
2016-12-27 12:23:03 +0800

    no message

---
+++ demo.php
@@ -0,0 +1,6 @@
+<?php
+require 'vendor/autoload.php';
+
+//$obj = new \houdunwang\request\Request();
+//echo $obj->ip();
+echo \houdunwang\request\Request::ip();
---
+++ src/Cart.php
@@ -0,0 +1,39 @@
+<?php
+/** .-------------------------------------------------------------------
+ * |  Software: [HDCMS framework]
+ * |      Site: www.hdcms.com
+ * |-------------------------------------------------------------------
+ * |    Author: 向军 <2300071698@qq.com>
+ * |    WeChat: aihoudun
+ * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
+ * '-------------------------------------------------------------------*/
+namespace houdunwang\cart;
+
+/**
+ * 购物车处理类
+ * Class Cart
+ * @package houdunwang\cart
+ */
+class Cart {
+	//连接
+	protected $link = null;
+
+	//更改缓存驱动
+	protected function driver() {
+		$this->link = new Query( $this );
+
+		return $this;
+	}
+
+	public function __call( $method, $params ) {
+		if ( is_null( $this->link ) ) {
+			$this->driver();
+		}
+
+		return call_user_func_array( [ $this->link, $method ], $params );
+	}
+
+	public static function __callStatic( $name, $arguments ) {
+		return call_user_func_array( [ new static(), $name ], $arguments );
+	}
+}
